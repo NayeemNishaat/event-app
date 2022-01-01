144f72d06f164c57bdf73c4be9fd9fbcdac6d04c
nayeemjc <RxNayeemIMI@gmail.com>
2022-01-02 02:23:36 +0600

    client-side rendering for server side checkout previous commit

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,31 +1,57 @@
 import { useRouter } from "next/router";
-import { getFilteredEvents } from "../../helpers/api-utils";
+import { useState, useEffect } from "react";
+// import { getFilteredEvents } from "../../helpers/api-utils";
 import EventList from "../../components/events/event-list";
 import ResultsTitle from "../../components/events/results-title";
 import Button from "../../components/ui/button";
 
-// Important: The slig only kicks in if there are more than one dynamic segment in the URL!
-
+// Important: The slug only kicks in if there are more than one dynamic segment in the URL!
 function FilteredEventsPage(props) {
-	// const router = useRouter();
+	const [loadedEvents, setLoadedEvents] = useState();
+	const router = useRouter();
+	const filteredData = router.query.slug;
 
-	// const filteredData = router.query.slug;
+	useEffect(() => {
+		// Remark: Suboptimal because filtering in the server is the best approach!
+		fetch(
+			"https://nextjs-prerendering-27c46-default-rtdb.asia-southeast1.firebasedatabase.app/events.json"
+		)
+			.then((res) => res.json())
+			.then((data) => {
+				if (data) {
+					const events = [];
+					for (const key in data) {
+						events.push({
+							id: key,
+							...data[key] // Note: Populating the array with objects!
+						});
+					}
+					setLoadedEvents(events);
+				}
+			});
+	}, []);
 
-	// if (!filteredData) {
-	// 	return (
-	// 		<>
-	// 			<p className="text-center text-4xl py-20">Loading!</p>;
-	// 			<div className="text-center">
-	// 				<Button link="/events">Show All Events</Button>
-	// 			</div>
-	// 		</>
-	// 	);
-	// }
+	if (!loadedEvents) {
+		return (
+			<>
+				<p className="text-center text-4xl py-20">Loading!</p>;
+				<div className="text-center">
+					<Button link="/events">Show All Events</Button>
+				</div>
+			</>
+		);
+	}
+	const year = +filteredData[0];
+	const month = +filteredData[1];
 
-	// const year = +filteredData[0];
-	// const month = +filteredData[1];
-
-	if (props.hasError) {
+	if (
+		isNaN(year) ||
+		isNaN(month) ||
+		year > 2030 ||
+		year < 2021 ||
+		month < 1 ||
+		month > 12
+	) {
 		return (
 			<>
 				<p className="text-center text-4xl py-20">Invalid Filter!</p>;
@@ -36,7 +62,15 @@ function FilteredEventsPage(props) {
 		);
 	}
 
-	const filteredEvents = props.events;
+	const filteredEvents = loadedEvents.filter((event) => {
+		const eventDate = new Date(event.date);
+		return (
+			eventDate.getFullYear() === year &&
+			eventDate.getMonth() === month - 1
+		);
+	});
+
+	// const filteredEvents = props.events;
 
 	if (!filteredEvents || filteredEvents.length === 0) {
 		return (
@@ -49,7 +83,8 @@ function FilteredEventsPage(props) {
 		);
 	}
 
-	const date = new Date(props.date.year, props.date.month - 1);
+	// const date = new Date(props.date.year, props.date.month - 1);
+	const date = new Date(year, month - 1);
 
 	return (
 		<>
@@ -60,44 +95,44 @@ function FilteredEventsPage(props) {
 }
 
 // Note: For slugs generally is better!
-export async function getServerSideProps(context) {
-	const { params } = context;
-	const filteredData = params.slug;
+// export async function getServerSideProps(context) {
+// 	const { params } = context;
+// 	const filteredData = params.slug;
 
-	const year = +filteredData[0];
-	const month = +filteredData[1];
+// 	const year = +filteredData[0];
+// 	const month = +filteredData[1];
 
-	if (
-		isNaN(year) ||
-		isNaN(month) ||
-		year > 2030 ||
-		year < 2021 ||
-		month < 1 ||
-		month > 12
-	) {
-		return {
-			props: {
-				hasError: true
-			}
-			// Point: Alternatives
-			// notFound: true
-			// redirect:{
-			// 	destination:"/error"
-			// }
-		};
-	}
+// 	if (
+// 		isNaN(year) ||
+// 		isNaN(month) ||
+// 		year > 2030 ||
+// 		year < 2021 ||
+// 		month < 1 ||
+// 		month > 12
+// 	) {
+// 		return {
+// 			props: {
+// 				hasError: true
+// 			}
+// 			// Point: Alternatives
+// 			// notFound: true
+// 			// redirect:{
+// 			// 	destination:"/error"
+// 			// }
+// 		};
+// 	}
 
-	const filteredEvents = await getFilteredEvents({ year, month });
+// 	const filteredEvents = await getFilteredEvents({ year, month });
 
-	return {
-		props: {
-			events: filteredEvents,
-			date: {
-				year,
-				month
-			}
-		}
-	};
-}
+// 	return {
+// 		props: {
+// 			events: filteredEvents,
+// 			date: {
+// 				year,
+// 				month
+// 			}
+// 		}
+// 	};
+// }
 
 export default FilteredEventsPage;
